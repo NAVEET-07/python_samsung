9e8f9a5bd541db922ca5e3017ac8b2f53ce2f15e
Fakkirappa Sunagar <mailarisunagartejus@gmail.com>
2024-12-11 11:18:49 +0530

    day3 notes updated

--- basic_buildind/problem8.py
+++ basic_buildind/problem8.py
@@ -6,3 +6,10 @@ if(a==1):
         print("go out with umbrella")
     else:
         print("go as you wish")
+elif(a==2):
+    b=input("1: sunny\n2: hot\n")
+    if(b==1):
+        print(" go out as you wish")
+    elif(b==2):
+        print("then go out with jacket")
+    
